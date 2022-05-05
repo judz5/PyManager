ec2e74a80ee9d239143012000c6435b9dac0f16d
Judson Salinas <judz1105@gmail.com>
2022-05-04 20:07:19 -0500

    done, now encryption

---
+++ data_control.py
@@ -0,0 +1,53 @@
+import psycopg2, subprocess
+
+def connect():
+    try:
+        connection = psycopg2.connect(user='judz', password='judz', host='127.0.0.1', database='password_storage')
+        return connection
+    except (Exception, psycopg2.Error) as error:
+        print(error)
+
+def send(password, email, username, url, app_name):
+    try:
+        connection = connect()
+        cursor = connection.cursor()
+        postgres_insert_query = """ INSERT INTO accounts (password, email, username, url, app_name) VALUES (%s, %s, %s, %s, %s)"""
+        record_to_insert = (password, email, username, url, app_name)
+        cursor.execute(postgres_insert_query, record_to_insert)
+        connection.commit()
+    except (Exception, psycopg2.Error) as error:
+        print(error)
+
+def find_pass(app_name):
+    try:
+        connection = connect()
+        cursor = connection.cursor()
+        postgres_find_query = """ SELECT email, username, password FROM accounts WHERE app_name = '""" + app_name + "'"
+        cursor.execute(postgres_find_query, app_name)
+        connection.commit()
+        pw = cursor.fetchone()
+        if not(pw==None):
+            print('Email : ',pw[0])
+            print('Username : ',pw[1])
+            print('Password : ',pw[2])
+            subprocess.run('pbcopy', universal_newlines=True, input=pw[2])
+            print("\nPassword Copied To Clipboard.")
+        else:
+            print('No Account Found')
+    
+    except (Exception(), pyscopg2.Error) as error:
+        print(error)
+
+def all_accounts():
+    try:
+
+        connection = connect()
+        cursor = connection.cursor()
+        postgres_find_query = """ SELECT app_name FROM accounts """
+        cursor.execute(postgres_find_query)
+        connection.commit()
+        app_names = cursor.fetchall()
+        return(app_names)
+
+    except (Exception(), pyscopg2.Error) as error:
+        print(error)
---
+++ menu_control.py
@@ -0,0 +1,70 @@
+import pyfiglet, re
+from data_control import *
+
+
+def title():
+    print(pyfiglet.figlet_format("PyManager", font = "slant"))
+
+def menu():
+    print(('-'*24),' Menu ',('-'*23))
+    print('1. Add Account')
+    print('2. Account Search')
+    print('3. All Accounts')
+    print('4. Exit')
+    print('-'*55)
+    select = input()
+    if(select == '1'):
+        add_account()
+    elif(select == '2'):
+        find_account()
+    elif(select == '3'):
+        account_list()
+    elif(select == '4'):
+        quit()
+    else:
+        print("ERROR... ABORTING")
+        quit()
+
+def add_account():
+    print(('-'*21)," Add Account ",('-'*21))
+    print("Welcome to the Account Adder!")
+    print("If You Do Not Wish to Answer A Field, Simply Press Enter.\n")
+    app_name = input('Enter Name of App / Website : ')
+    url = input('Enter the Website URL : ')
+    email = input('Enter Account Email : ')
+    username = input('Enter Account Username : ')
+    password = input('Enter Account Password : ')
+    send(password, email, username, url, app_name)
+    print("Done!")
+    menu()
+
+def find_account():
+    print(('-'*19)," Account Search",('-'*19))
+    app_name = input("Enter Name of App / Website : ")
+    print("Searching...\n")
+    find_pass(app_name)
+    menu()
+
+def account_list():
+    print(('-'*20)," Account List ",('-'*19))
+    print()
+    accounts = all_accounts()
+
+    keyList = {}
+
+    for i, account in enumerate(accounts):
+        formatted = re.split(r"'(.*?)'", str(account))
+        print(i+1,".",formatted[1],"     ")
+        keyList[str(i+1)] = formatted[1]
+
+    choice = input("\nEnter the Number of the Account You Want to View : ")
+
+    print(('-'*20)," Account Info ",('-'*19))
+
+    find_pass(keyList.get(choice))
+    menu()
+
+
+
+    
+
